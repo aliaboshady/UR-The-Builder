6e26c515164cdb40ac937a44b3a958cb48fa912c
Ali Aboshady <ali_aboshady@hotmail.com>
2021-06-04 15:09:42 +0100

    Created input manager script to check mouse position and instantiate cubes

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public LayerMask mouseInputMask;
+	public GameObject buildingPrefab;
+	public int cellSize = 3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Update()
+	{
+		GetInput();
+	}
+
+	public void GetInput()
+	{
+		bool leftMousePressed = Input.GetMouseButtonDown(0);
+		bool mouseOverUI = EventSystem.current.IsPointerOverGameObject();
+
+		if (leftMousePressed && !mouseOverUI)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			bool didHit = Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask);
+
+			if (didHit)
+			{
+				Vector3 position = hit.point - transform.position;
+				CreateBuilding(CalculateGridPosition(position));
+			}
+		}
+	}
+
+	public Vector3 CalculateGridPosition(Vector3 inputPosition)
+	{
+		int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
+		int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+		return new Vector3(x * cellSize, 0, z * cellSize);
+	}
+
+	void CreateBuilding(Vector3 gridPosition)
+	{
+		Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+	}
 }
